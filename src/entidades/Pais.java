ad0c7a23624fd7b5510812acc5c5e3c63132490e
Joako9780 <57472877+Joako9780@users.noreply.github.com>
2025-10-21 02:21:11 -0300

    Update Pais.java

--- src/entidades/Pais.java
+++ src/entidades/Pais.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class Pais {
     private Long id;
+    private String nombre;
 
     @Builder.Default
     private Set<Provincia> provincias = new HashSet<>();
